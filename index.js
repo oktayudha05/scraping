1d791bdff2cabd272949643c4441c1ca84a939fb
oktayudha05 <oktayudha05@gmail.com>
2023-09-18 13:21:12 +0700

    ganti user agent

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const puppeteer = require('puppeteer');
     executablePath: 'C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe',
     headless: false  // Setel ke true jika ingin menjalankan dalam mode headless
   });
-
+  
+  await page. setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36')
   const page = await browser.newPage()
-  await page.goto('https://shopee.co.id/')
+  await page.goto('https://shopee.co.id/Kabel-Data-Macaron-Micro-Usb-Fast-Charging-Data-Cable-V8-Kabel-Data-Warna-Warni-i.178227098.14474910378')
 
   // Lakukan interaksi dan manipulasi sesuai kebutuhan
-  await page.waitForSelector('.nama_kelas')
-  await page.click('.kmYTt8') // Mengklik tombol dengan id "myButton"
+  // await page.waitForSelector('.nama_kelas')
+  // await page.click('.shopee-popup__close-btn')// Mengklik tombol dengan id "myButton"
 
 
 //   await browser.close()
